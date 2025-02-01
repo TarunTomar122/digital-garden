5c738cd2f9f1b1a91ed2104d392b418108a355b2
TarunAdobe <ttomar@adobe.com>
2025-02-01 11:20:55 +0530

    chore: add the loading animation to like button

--- v2/my-garden/src/components/LikeButton/LikeButton.tsx
+++ v2/my-garden/src/components/LikeButton/LikeButton.tsx
@@ -98,7 +98,7 @@ export default function LikeButton({ id, type }: LikeButtonProps) {
             onClick={handleLike}
             className={`flex items-center gap-1.5 py-1 px-2 rounded-full transition-all duration-300 ease-in-out self-start bg-slate-800/30 backdrop-blur-lg 
                 ${liked ? 'text-rose-200 scale-105' : 'text-slate-400 hover:text-rose-100'} 
-                ${loading ? 'opacity-90 animate-pulse' : ''}
+                ${loading ? 'opacity-50 animate-pulse backdrop-blur-lg text-rose-300' : ''}
             `}
             aria-label={liked ? 'Unlike' : 'Like'}
             disabled={loading}
@@ -108,7 +108,7 @@ export default function LikeButton({ id, type }: LikeButtonProps) {
                 viewBox="0 0 24 24"
                 fill={liked ? 'currentColor' : 'none'}
                 stroke="currentColor"
-                className="w-4 h-4 transition-transform duration-300 ease-in-out transform hover:scale-110"
+                className={`w-4 h-4 transition-transform duration-300 ease-in-out transform hover:scale-110 ${loading? 'animate-pulse text-rose-300' : ''}`}
                 strokeWidth={liked ? '0' : '1.5'}
             >
                 <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
